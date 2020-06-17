a420d48827e7aa20e85b53fe3a81949ca3f84cfc
변지윤 <>
2020-06-17 11:17:29 +0900

    test commit

---
+++ redux/src/components/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Counter.css'
+
+class Counter extends Component {
+
+    render() {
+        let { onIncrement, onDecrement, onSetColor, color, number } = this.props;
+        return (
+            <div className="Counter"
+                onClick={onIncrement}
+                onContextMenu={(e) => {
+                    e.preventDefault();
+                    onDecrement();
+                }}
+                onDoubleClick={onSetColor}
+                style={{ backgroundColor: color }}
+            > {number}
+            </div>
+        )
+    }
+}
+
+// props 타입 확인
+Counter.propTypes = {
+    number: PropTypes.number,
+    color: PropTypes.string,
+    onIncrement: PropTypes.func,
+    onDecrement: PropTypes.func,
+    onSetColor: PropTypes.func,
+}
+
+// props 기본값 설정
+Counter.defaultProps = {
+    number: 0,
+    color: 'black',
+    onIncrement: () => console.log('onIncrement not Defined'),
+    onDecrement: () => console.log('onDecrement not Defined'),
+    onSetColor: () => console.log('onSetColor not Defined'),
+};
+
+export default Counter;
