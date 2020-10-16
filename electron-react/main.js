74763646805e40df91f9d1e29efd1e56f0fdf90e
변지윤 <>
2020-10-16 09:44:12 +0900

    -

--- electron-react/main.js
+++ electron-react/main.js
@@ -1,7 +1,40 @@
 const { app, BrowserWindow, ipcMain, BrowserView, shell, net } = require("electron")
 const path = require("path");
 const { session } = require('electron')
+const http = require('http')
+const express = require('express')()
+const cookieParser = require("cookie-parser")
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const httpProxy = require('http-proxy');
+const { runInNewContext } = require("vm");
 
+express.listen(3001)
+// http.createServer((res, req) => {
+//     proxy.web(req, res, { target: 'http://dev.wehagot.com' });
+// })
+// express.listen(3001)
+// appServer.use('/', proxy('http://dev.wehagot.com', {
+//     proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+//       proxyReqOpts.headers['cookie'] = 'locale=ko; _ga=GA1.2.25191625.1594253657; _gid=GA1.2.145365850.1594253657; _gat=1; AUTH_A_TOKEN=xmFtFkZbnnbPoQtl6UeUSbpsIXIb9s; AUTH_R_TOKEN=wosHdwx1znY5l4wdM3ZpZlpdSER7B4; wehago_s=313920826066322287273693270202585526914; h_selected_company_no=26928; cell_company_no=26928; h_portal_id=zcvfcat; h_selected_company_code=biz202003200000084; h_selected_employee_no=306054';
+//       return proxyReqOpts;
+//     }
+// }));        
+express.use(
+    createProxyMiddleware({
+        target: 'http://dev.wehagot.com/',
+        changeOrigin: true,
+        onProxyRes: (proxyRes, req, res) => {
+            res.cookie('COOKIE', 'DEL')
+            res.cookie("CHANGE", 'DOMAIN' , {domain: ".wehago.com"})
+        },
+        onProxyReq: (proxyReq, req, res) => {
+
+        },
+        onProxyReqWs: (proxyReq, req, socket, options, head) => {
+    
+          }
+    }),
+)
 
 let win;
 let view;
@@ -57,10 +90,37 @@ ipcMain.on('dialog', (e, data) => {
 })
 
 ipcMain.on("link", () => {
-    let newWin = new BrowserWindow({ width: 800, height: 1000 })
-    newWin.on('close', function () { win = null })
-    newWin.loadURL("https://dev.wehagot.com")
-    newWin.show()
+
+    // express.use((req, res, next) => {
+    //     res.cookie('cookies', 'delicious')
+    //     res.redirect('https://www.google.com')
+    //     // next();
+    // })
+    
+// express.post('/', (req, res) => {
+//   res.cookie('favorite', "cookie");
+//   res.redirect('https://www.google.com')
+// });
+
+    // express.post('https://www.google.com',function(req,res){
+    //     res.cookie('cookies', 'delicious')
+    //     res.redirect('https://www.google.com')
+    //   });
+
+    // http.createServer((req, res) => {
+    //     console.log("req", req.url)
+    //     res.writeHead(200, {'Set-Cookie' : 'Cookies=delicious'})
+    //     res.end("hoeeee")
+
+    // })
+    // .listen(3001, () => {
+        shell.openExternal('http://localhost:3001')
+    // })
+
+    // const appServer = require('express')()
+    // const http = require('http')
+    // const proxy = require('express-http-proxy');
+
 })
 
 const createDialog = (data, param) => {
